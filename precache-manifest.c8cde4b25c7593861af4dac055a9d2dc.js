38e4a58f41359af17c7dce57edf0cfb5d783f8e6
seung hee shin <department34@naver.com>
2020-07-06 21:30:12 +0900

    Updates

---
+++ precache-manifest.c8cde4b25c7593861af4dac055a9d2dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c981b6301b49388c1994351095ca59",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b4b72eb1cfbc3c0a0912",
+    "url": "/movie_app/static/css/main.41071d32.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b72eb1cfbc3c0a0912",
+    "url": "/movie_app/static/js/main.b757bc41.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
